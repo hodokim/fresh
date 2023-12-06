b1fff07f02cc5c63668074a0338551dec4e209c2
baha.kim <bahakim@shinsegae.com>
2023-12-07 08:09:52 +0900

    반복문(map)을 활용하여 html 태그 그리기

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,23 @@
+// import Image from "next/image" 이미지 최적화는 이렇게 해볼 수 있다.
+// <Image></Image> 를 활용하며 반드시 태그 안에 width, height을 명시해줘야 한다.
+// 또한 next.config.js 내에서 별도의 설정이 필요하다. (자세한건 구글링을 통해 참조하자.)
+
 export default function List() {
+    let products = ["Tomatoes", "Pasta", "Coconut"];
+
     return (
         <div>
             <h2>Products</h2>
-            <div className="food">
-                <h4>상품명 $40</h4>
-            </div>
-            <div className="food">
-                <h4>상품명 $40</h4>
-            </div>
+            {
+                products.map((product, idx)=>{
+                    return (
+                      <div className="food" key={idx}>
+                        <img src={`/food${idx}.png`} className="food-img"/>
+                        <h4>{product} $40</h4>
+                      </div>
+                    );
+                })
+            }
         </div>
     )
 }
